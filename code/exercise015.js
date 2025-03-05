7895637a08ea74ece38abc534cdb93473e6ecae5
Scotny <marsithb2022@gmail.com>
2025-03-05 15:05:47 +0500

    8 kyu L1: Bartender, drinks!

---
+++ code/exercise015.js
@@ -0,0 +1,30 @@
+function getDrinkByProfession(profession) {
+  switch (profession.toLowerCase()) {
+    case "jabroni":
+      return "Patron Tequila";
+    case "school counselor":
+      return "Anything with Alcohol";
+    case "programmer":
+      return "Hipster Craft Beer";
+    case "bike gang member":
+      return "Moonshine";
+    case "politician":
+      return "Your tax dollars";
+    case "rapper":
+      return "Cristal";
+    default:
+      return "Beer";
+  }
+}
+
+// const drinks = {
+//   jabroni: "Patron Tequila",
+//   "school counselor": "Anything with Alcohol",
+//   programmer: "Hipster Craft Beer",
+//   "bike gang member": "Moonshine",
+//   politician: "Your tax dollars",
+//   rapper: "Cristal",
+// };
+
+// const getDrinkByProfession = (profession) =>
+//   drinks[profession.toLowerCase()] || "Beer";
